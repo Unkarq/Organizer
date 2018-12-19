26f97e9deb6b497c90a57f0ab01b53dc4c4f76d0
Unkarq <unkar91@gmail.com>
2018-12-19 18:37:55 +0100

    Etap 1

---
+++ src/main/java/com/sda/gda17/organizer/organizer/OrganizerApplication.java
@@ -0,0 +1,14 @@
+package com.sda.gda17.organizer.organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganizerApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/sda/gda17/organizer/organizer/OrganizerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sda.gda17.organizer.organizer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrganizerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
